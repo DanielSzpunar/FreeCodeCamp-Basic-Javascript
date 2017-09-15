4d6d141a925a91aefb65b442b468babb9a701414
DanielSzpunar <danielszpunar@gmail.com>
2017-09-15 17:55:20 -0400

    javascript

---
+++ 021convertCelsiusToFahrenheit.js
@@ -0,0 +1,11 @@
+function convertToF(celsius) {
+    var fahrenheit = celsius * 9/5 + 32;
+    // Only change code below this line
+    
+    
+    // Only change code above this line
+    return fahrenheit;
+  }
+  
+  // Change the inputs below to test your code
+  convertToF(30);
