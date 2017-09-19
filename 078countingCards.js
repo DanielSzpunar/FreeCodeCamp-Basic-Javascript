790d3849fc01a7fb4971bc226398f7f0cd5bffc2
DanielSzpunar <danielszpunar@gmail.com>
2017-09-19 10:59:58 -0400

    javascript

--- 078countingCards.js
+++ 078countingCards.js
@@ -1,39 +1,43 @@
 var count = 0;
 
 function cc(card) {
-  // Only change code below this line
-  switch(card) {
-    case 2:
-    case 3:
-    case 4:
-    case 5:
-    case 6:
-      count+=1;
-      break;
-    case 7:
-    case 8:
-    case 9:
-      count+=0;
-      break;
-    case 10:
-    case "J":
-    case "Q":
-    case "K":
-    case "A":
-      count-=1;
-      break;
-  }
-  
-  if (count >= 1)
-    return count + " Bet";
-    else if (count <= 0) {
-      return count + " Hold";
+    // Only change code below this line
+    switch (card) {
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+        case 6:
+            count += 1;
+            break;
+        case 7:
+        case 8:
+        case 9:
+            count += 0;
+            break;
+        case 10:
+        case "J":
+        case "Q":
+        case "K":
+        case "A":
+            count -= 1;
+            break;
     }
-  
-  return count;
-  // Only change code above this line
+
+    if (count >= 1)
+        return count + " Bet";
+    else if (count <= 0) {
+        return count + " Hold";
+    }
+
+    return count;
+    // Only change code above this line
 }
 
 // Add/remove calls to test your function.
 // Note: Only the last will display
-cc(2); cc(3); cc(7); cc('K'); cc('A');
+cc(2);
+cc(3);
+cc(7);
+cc('K');
+cc('A');
