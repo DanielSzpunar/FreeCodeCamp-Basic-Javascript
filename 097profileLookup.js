6438eda4a53f67516b4c3bc2d2351db81a78884b
DanielSzpunar <danielszpunar@gmail.com>
2017-09-19 20:33:41 -0400

    javascript

---
+++ 097profileLookup.js
@@ -0,0 +1,38 @@
+var contacts = [{
+        "firstName": "Akira",
+        "lastName": "Laine",
+        "number": "0543236543",
+        "likes": ["Pizza", "Coding", "Brownie Points"]
+    },
+    {
+        "firstName": "Harry",
+        "lastName": "Potter",
+        "number": "0994372684",
+        "likes": ["Hogwarts", "Magic", "Hagrid"]
+    },
+    {
+        "firstName": "Sherlock",
+        "lastName": "Holmes",
+        "number": "0487345643",
+        "likes": ["Intriguing Cases", "Violin"]
+    },
+    {
+        "firstName": "Kristian",
+        "lastName": "Vos",
+        "number": "unknown",
+        "likes": ["Javascript", "Gaming", "Foxes"]
+    }
+];
+
+function lookUpProfile(firstName, property) {
+    for (var i = 0; i < contacts.length; i++) {
+        if (contacts[i].firstName === firstName) {
+            if (contacts[i].hasOwnProperty(property)) {
+                return contacts[i][property];
+            } else {
+                return "No such property";
+            }
+        }
+    }
+    return "No such contact";
+}
